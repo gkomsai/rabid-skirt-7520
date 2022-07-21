807fb45b514c3f1d5a28e363ae3663e0f1673fba
Gautam Kumar <gkomsai788@gmail.com>
2022-07-21 10:40:56 +0530

    Revert changes
    This reverts commit 1542b46c7028fb1299e4355262ca039eda9fa090.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import { Box, Image, VStack } from "@chakra-ui/react";
+import React from "react";
+import Carousel from "../components/Carousel";
 
 const Home = () => {
   return (
-    <div>
-    <h1>Welcome to the home page!</h1>
-    </div>
-  )
-}
+    <Box>
+      <Box>
+        <Carousel />
+      </Box>
+    </Box>
+  );
+};
 
-export default Home
+export default Home;
